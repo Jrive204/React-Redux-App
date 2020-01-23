333856b550f65b030b62c369ed0523f6882163b5
Jose <Jrivera6869@yahoo.com>
2020-01-23 15:34:07 -0500

    added stuff

---
+++ async-redux-project/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>APP</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
